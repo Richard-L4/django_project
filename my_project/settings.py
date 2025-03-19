46fb9b38690cf27cb054e7b7d66996d4a6028c69
Richard-L4 <rwells945@gmail.com>
2025-03-19 11:09:21 +0000

    files added

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-si8s2_9pg0wdrybpy%@wbq3vcqlq+2^e-9aul=3pqjn&=91xt@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
