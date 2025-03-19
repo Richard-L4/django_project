ce608552b323356a831bc99e82714c8c833eee1a
Richard-L4 <rwells945@gmail.com>
2025-03-19 10:57:41 +0000

    files added

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-si8s2_9pg0wdrybpy%@wbq3vcqlq+2^e-9aul=3pqjn&=91xt@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
